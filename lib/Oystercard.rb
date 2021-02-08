cae87d04e02861be9848f4760ca8ee229caea365
Adam Jonas <adamjonas25@gmail.com>
2021-02-08 17:29:12 +0000

    refactor methods

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,23 +1,16 @@
 class Oystercard
-  attr_reader :balance, :limit
-  attr_reader :entry_station
+  attr_reader :balance, :limit, :entry_station
+
   MAXIMUM_LIMIT = 90
   MINIMUM_FARE = 1
 
-  def initialize
-    @balance = 0
-    @limit = MAXIMUM_LIMIT
-    @in_journey = false
-    @entry_station = entry_station
-  end
-
   def top_up(amount)
+    top_up_limit_guard(amount)
     self.balance += amount
-    fail "#{amount} is over limit!" if over_limit?(amount)
   end
 
   def touch_in(station)
-    fail 'Not enough money' if low_balance?
+    low_balance_guard
     update_journey_status
     self.entry_station = station
   end
@@ -31,16 +24,31 @@ class Oystercard
 
   attr_writer :balance, :entry_station
   attr_accessor :in_journey
-  
+
   alias :in_journey? :in_journey
   undef :in_journey
 
-  def over_limit?(amount)
-    amount + balance > limit
+  def initialize
+    @balance = 0
+    @limit = MAXIMUM_LIMIT
+    @in_journey = false
+    @entry_station = nil
+  end
+
+  def low_balance_guard
+    fail 'Not enough money' if low_balance?
   end
 
   def low_balance?
     balance < MINIMUM_FARE
+  end
+
+  def top_up_limit_guard(amount)
+    fail "#{amount} is over limit!" if over_limit?(amount)
+  end
+
+  def over_limit?(amount)
+    amount + balance > limit
   end
 
   def deduct(amount)
